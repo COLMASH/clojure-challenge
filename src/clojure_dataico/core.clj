f9ec645edb2a4b438e58035bc8094e3888549cde
Miguel Santana <miguel@savesolarcorporation.com>
2023-03-14 02:37:19 -0500

    remove unused and commented code

--- src/clojure_dataico/core.clj
+++ src/clojure_dataico/core.clj
@@ -3,18 +3,6 @@
 
 (use 'clojure-dataico.invoice)
 
-;; (defn find-invoice-items [invoice]
-;;   (println "Solution to Problem 1: Those are the items 📦 that satisfy the conditions asked")
-;;   (println (->> invoice
-;;        :invoice/items
-;;        (filter (fn [item]
-;;                  (some #(= (:tax/category %) :iva) (:taxable/taxes item))))
-;;        (filter (fn [item]
-;;                  (not (some #(= (:retention/category %) :ret_fuente) (:retentionable/retentions item)))))
-;;        (filter (fn [item]
-;;                  (or (some #(= (:tax/rate %) 19) (:taxable/taxes item))
-;;                      (some #(= (:retention/rate %) 1) (:retentionable/retentions item))))))))
-
 (defn find-invoice-items [invoice]
   (println "Solution to Problem 1: Those are the items 📦 that satisfy the conditions asked")
   (println (->> invoice
@@ -25,8 +13,6 @@
                 (remove (fn [item] 
                           (and (some #(= (:tax/rate %) 19) (:taxable/taxes item))
                                (some #(= (:retention/rate %) 1) (:retentionable/retentions item))))))))
-
-;; (find-invoice-items invoice)
 
 (defn -main
   [& args]
