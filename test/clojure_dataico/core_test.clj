d548ae12dd33bd44cb351a3934614dc2c3ab771b
Miguel Santana <miguel@savesolarcorporation.com>
2023-03-13 21:46:02 -0500

    first commit

---
+++ test/clojure_dataico/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-dataico.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-dataico.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
